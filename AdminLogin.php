58dfc0b0c6cde0e93e339f9ac545cd10205899aa
Marcelina Teodoro <61193617+marcelateo@users.noreply.github.com>
2021-02-19 22:29:46 +0000

    Update login page to forward to index
    updated the login page with post method to validate user and forward to index home page

--- AdminLogin.php
+++ AdminLogin.php
@@ -11,15 +11,16 @@ require("connection.php");
 
 <div class="login-form">
 <h2>ADMIN LOGIN PANEL</h2>
+
 <form method="POST">
 <div class="input-field">
 <i class="fas fa-user"></i>
-<input type="text" placeholder="Admin Name" name="username">
+<input type="text" placeholder="Admin Name" name="AdminName">
 </div>
 
 <div class="input-field">
 <i class="fas fa-lock"></i>
-<input type="password" placeholder="Password" name="password">
+<input type="password" placeholder="Password" name="AdminPassword">
 </div>
 
 <button type="submit" name= "Signin">Sign In</button>
@@ -33,19 +34,19 @@ require("connection.php");
 <?php
 if(isset($_POST['Signin']))
 {
-	$query="SELECT * FROM 'adminuser' WHERE 'username'='$_POST[username]' AND 'password'= '$_POST[password]'";
-	$result=msqli_query($con,$query);
+	$query=" SELECT * FROM `admin_login` WHERE `Admin_Name`= '$_POST[AdminName]' AND `Admin_Password`= '$_POST[AdminPassword]'";
+    $result=mysqli_query($con,$query);
 	if(mysqli_num_rows($result)==1)
 	{
-		session_start();
-		$_SESSION['AdminLoginId']=$_POST['username'];
-		header("location: AdminPanel.php");
+		
+	session_start();
+		$_SESSION['AdminLoginId']=$_POST['AdminName'];
+		header("location:index.php");
 	} else
 	{
 		echo "<script>alert('Incorrect Password'); </script>";
 	}
 }
-
 ?>
 
 </body>
