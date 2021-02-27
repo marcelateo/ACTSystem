6d82d23cbceaf8976904a91d77a0a542ac18b76c
Marcelina Teodoro <61193617+marcelateo@users.noreply.github.com>
2021-02-27 12:54:24 +0000

    Update Room.php
    improved the post method

--- Room.php
+++ Room.php
@@ -1,28 +1,29 @@
 <html>
 
 <head>
+
 <?php
 error_reporting(0);
-include 'connection.php'
-$roomname = $_POST['roomname'];
-$floor= $_POST['floor'];
-$sql= "INSERT into rooms (roomname, floor ) values ('$roomname', '$floor')";
+include 'connection.php';
+$name =$_POST['roomname'];
+$floor = $_POST['floor'];
+$sql = "INSERT into rooms (roomname, floor) values ('$name', '$floor')";
 
-if ($_POST['submit']){
- if (mysqli_query()$con,$sql)){
- echo "Data added successfully"
- } else{
- echo "Something went wrong"
-}
-}
-<?
-
+	if ($_POST['submit']){
+		if (mysqli_query($con, $sql)){
+			echo "Data added successfully";
+		}
+		else{
+			echo "Something went wrong";
+		}
+	}
+?>
 </head>
 
 <body>
 <form action="Room.php" method= "POST">
-Name: <input type="text" name="roomname">
-Floor: <input type="text" name="floor">
+ Room Name: <input type="text" name="roomname">
+ Room floor: <input type="text" name="floor">
 <input type="submit" name= "submit" value="Send Info">
 
 </form>
