0a2ac88db4f942956a26fd7c9525cdb03c56b6f8
Marcelina Teodoro <61193617+marcelateo@users.noreply.github.com>
2021-03-06 17:59:44 +0000

    Update Schedule.php
    updated the page to support CRUD options in one page

--- Schedule.php
+++ Schedule.php
@@ -1,35 +1,72 @@
-<html>
-<head>
-<?php
-error_reporting(0);
+<?php 
 include 'connection.php';
-$name =$_POST['eventtype'];
-$responsible = $_POST['responsible'];
-$sql = "INSERT into events (eventtype, responsible, datetime) values ('$eventype', '$responsible', 'datetime')";
-
-	if ($_POST['submit']){
-		if (mysqli_query($con, $sql)){
-			echo "Data added successfully";
-		}
-		else{
-			echo "Something went wrong";
-		}
-	}
 ?>
 
+<html>
+<head>
+<title>Events Schedule</title>    
+<link rel="stylesheet" type="text/css" href="stylepage.css"> 
+    
 </head>
-
 <body>
-
-<form action="Schedule.php" method= "POST">
-
-Event: <input type= "text" name="eventtype">
-Lecturer/Staff: <input type= "text" name="responsible">
-Data: <input type= "date" name="datetime">
-<input type="submit" name="submit" value="Send Info">
-
-</form>
-
+  <table>
+    <thead>
+      <tr>
+        <th>Events</th>
+        <th>Type</th>
+        <th>Date/Time</th>
+          <th>Room Name</th>
+          <th>Staff Name</th>
+        <th colspan="5">Action</th>
+        </tr>
+      </thead>
+    <tbody>
+      <tr>
+        <td>Network and Switching</td>
+        <td>Regular Class</td>
+        <td>14/02/2021 15:00:00</td>
+          <td>Yale</td>
+          <td>Greg</td>
+        <td>
+          <a href="#">Edit</a>
+          </td>
+          <td>
+          <a href="#">Delete</a>
+          </td>
+        </tr>    
+      </tbody>
+    </table>  
+    <form method= "post" action= "#">
+    <div class="input-group">
+        <label>Event</label>
+        <input type="text" name="event">
+        </div>
+         <div class="input-group">
+        <label>Type</label>
+        <input type="text" name="eventtype">
+        </div>
+         <div class="input-group">
+        <label>Datetime</label>
+        <input type="text" name="datetime">
+        </div>
+        <div class="input-group">
+        <label>Room Name</label>
+        <input type="text" name="roomname">
+        </div>
+        <div class="input-group">
+        <label>Staff Name</label>
+        <input type="text" name="staffName">
+        </div>
+         <div class="input-group">
+        <button type= "submit" name= "save" class="btn">Save</button>        
+        </div>
+    </form>
+    
+     <div>
+    <a href="index.php" class="button">Home</a>
+    
+    </div>
+    
 </body>
 
 
