439a37b3117d2c93fc2e8d28a073f11c20c70ac5
Marcelina Teodoro <61193617+marcelateo@users.noreply.github.com>
2021-03-14 23:46:40 +0000

    Update server2.php
    added query for edit and delete buttons for schedule.php

--- server2.php
+++ server2.php
@@ -6,7 +6,7 @@ $datetime= "";
 $roomID= "";
 $staffID = "";
 $id= 0;
-//$edit_state= false;
+$edit_state=false;
 
 //connect to database
 $db=mysqli_connect("localhost", "root", "", "actsystem");
@@ -19,13 +19,34 @@ $datetime= $_POST['datetime'];
 $roomID= $_POST['roomID'];
 $staffID= $_POST['staffID'];
 
- $query = "INSERT INTO `events`( `event`, `type`, `datetime`, `roomID`, `staffID`) VALUES ('$event','$type','$datetime', '$roomID', 'staffID')";
+ $query = "INSERT INTO events (event, type, datetime,roomID, staffID) VALUES ('$event','$type','$datetime', '$roomID', 'staffID')";
  mysqli_query($db, $query);
  header('location: Schedule.php');
  
 }
 
 
+if(isset($_POST['update'])){
+	$event = $_POST['event'];	
+	$type= $_POST['type'];
+	$datetime=$_POST['datetime'];
+	$roomID=$_POST['roomID'];
+	$staffID=$_POST['staffID'];
+	$id=$_POST['id'];
+
+
+	mysqli_query($db, "UPDATE events SET event= '$event', type= '$type', datetime= '$datetime' , roomID= '$roomID', staffID= '$staffID' WHERE id=$id");
+	header ('location: Schedule.php'); 
+	
+}
+//delete records
+if (isset ($_GET['del'])){
+	$id = $_GET['del'];
+	mysqli_query($db, "DELETE FROM events WHERE id=$id");
+	header ('location: Schedule.php'); 
+}
+
+//retrieve data from database
 $results = mysqli_query($db, "SELECT * FROM events");
 
 ?>
