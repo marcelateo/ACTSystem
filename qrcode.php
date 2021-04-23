6051033a3adabc3fd0469bd771a0e5188dc41993
Marcelina Teodoro <61193617+marcelateo@users.noreply.github.com>
2021-04-23 14:01:17 +0100

    Update qrcode.php

--- qrcode.php
+++ qrcode.php
@@ -1,10 +1,9 @@
 <?php
 //connect to database
-$db=mysqli_connect("localhost", "root", "", "actsystem");
+$db=mysqli_connect("remotemysql.com", "VC1cXIVdFt", "KQZUN1FPqb", "VC1cXIVdFt");
 $query= "SELECT * FROM rooms";
 
 $result1= mysqli_query($db, $query);
-
 ?>
 
 <html>
@@ -14,20 +13,30 @@ $result1= mysqli_query($db, $query);
 
 <body>
 
-<form method= "post">
+<form method="post" action="./qrresult.php">
 <center>
 <label>Please select a room to create a UNIQUE QR Code: </label>
 <select name= "roomName">
 <?php while ($row1 = mysqli_fetch_array($result1)): ?>
 
-<option><?php echo $row1[1];?></option>
+<option value="<?php echo $row1[1];?>"><?php echo $row1[1];?></option>
 <?php endwhile;?>
 </select>
 <button type= "submit" name="generate"> Generate QR Code</button>
 </center>
 </form>
+<!--<img src="./qrresult.php" height="500" width="500"/>-->
+     <div>
+    <!-- home button -->
+    <a href="index.php" class="button">Home</a>
+    
+    </div>
+    
 </body>
 
+<?php 
+https://chart.googleapis.com/chart?chs=300x300&cht=qr&chl=VALUE_GOES_HERE&choe=UTF-8
 
+?>
 
 </html>
