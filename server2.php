8a26b9d15b9b5f08a44ae4cbc892ab4debf3e3a0
Marcelina Teodoro <61193617+marcelateo@users.noreply.github.com>
2021-04-23 14:03:57 +0100

    Update server2.php

--- server2.php
+++ server2.php
@@ -3,23 +3,23 @@
 $event = "";
 $type= "";
 $datetime= "";
-$roomID= "";
+$roomid= "";
 $staffID = "";
 $id= 0;
 $edit_state=false;
 
 //connect to database
-$db=mysqli_connect("localhost", "root", "", "actsystem");
+$db=mysqli_connect("remotemysql.com", "VC1cXIVdFt", "KQZUN1FPqb", "VC1cXIVdFt");
 
 //if save button is clicked it save the data
 if (isset($_POST['save'])){
 $event = $_POST['event'];
 $type= $_POST['type'];
 $datetime= $_POST['datetime'];
-$roomID= $_POST['roomID'];
+$roomid= $_POST['roomid'];
 $staffID= $_POST['staffID'];
 
- $query = "INSERT INTO events (event, type, datetime,roomID, staffID) VALUES ('$event','$type','$datetime', '$roomID', 'staffID')";
+ $query = "INSERT INTO events (event, type, datetime,roomID, staffID) VALUES ('$event','$type','$datetime', '$roomid', 'staffID')";
  mysqli_query($db, $query);
  header('location: Schedule.php');
  
@@ -30,12 +30,12 @@ if(isset($_POST['update'])){
 	$event = $_POST['event'];	
 	$type= $_POST['type'];
 	$datetime=$_POST['datetime'];
-	$roomID=$_POST['roomID'];
+	$roomID=$_POST['roomid'];
 	$staffID=$_POST['staffID'];
 	$id=$_POST['id'];
 
 
-	mysqli_query($db, "UPDATE events SET event= '$event', type= '$type', datetime= '$datetime' , roomID= '$roomID', staffID= '$staffID' WHERE id=$id");
+	mysqli_query($db, "UPDATE events SET event= '$event', type= '$type', datetime= '$datetime' , roomid= '$roomid', staffID= '$staffID' WHERE id=$id");
 	header ('location: Schedule.php'); 
 	
 }
