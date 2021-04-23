a06cbdd1c3c57592eb23107d134b8ca1aba88f8e
Marcelina Teodoro <61193617+marcelateo@users.noreply.github.com>
2021-04-23 13:42:23 +0100

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,14 +1,13 @@
 <?php
-$server = "localhost";
-$username = "root";
-$password = "";
-$dbname = "actsystem";
-$con=mysqli_connect("localhost", "root", "", "actsystem");
 
+$con=mysqli_connect("remotemysql.com", "VC1cXIVdFt", "KQZUN1FPqb", "VC1cXIVdFt");
+
+//ckeck connection
 if(mysqli_connect_error())
 {
 	echo "Can not connect";
 }
+
 
 ?>
 
