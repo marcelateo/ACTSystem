6fdad46b31924e52cabb6a34b38b17e62ac1b79f
Marcelina Teodoro <61193617+marcelateo@users.noreply.github.com>
2021-05-12 00:18:21 +0100

    Update qrcode.php

--- qrcode.php
+++ qrcode.php
@@ -11,31 +11,28 @@ $result1= mysqli_query($db, $query);
 <title>QR Code </title>
 </head>
 
-<body>
+<body style="margin: 0; border: 0">
+    <?php include "sidebar.php"; ?>
+    <div style="height: 700px; font-family: Helvetica; margin-left: 350px;">
+        <form method="post" action="./qrresult.php">
+            <center>
+                <label>Please select a room to create a UNIQUE QR Code: </label>
+                <select name="roomName">
+                    <?php while ($row1 = mysqli_fetch_array($result1)) : ?>
 
-<form method="post" action="./qrresult.php">
-<center>
-<label>Please select a room to create a UNIQUE QR Code: </label>
-<select name= "roomName">
-<?php while ($row1 = mysqli_fetch_array($result1)): ?>
-
-<option value="<?php echo $row1[1];?>"><?php echo $row1[1];?></option>
-<?php endwhile;?>
-</select>
-<button type= "submit" name="generate"> Generate QR Code</button>
-</center>
-</form>
-<!--<img src="./qrresult.php" height="500" width="500"/>-->
-     <div>
-    <!-- home button -->
-    <a href="index.php" class="button">Home</a>
-    
+                        <option value="<?php echo $row1[1]; ?>"><?php echo $row1[1]; ?></option>
+                    <?php endwhile; ?>
+                </select>
+                <button type="submit" name="generate"> Generate QR Code</button>
+            </center>
+        </form>
+        <img src="./qrresult.php" height="500" width="500" />
     </div>
-    
+    <?php include "footer.php"; ?>
 </body>
 
-<?php 
-https://chart.googleapis.com/chart?chs=300x300&cht=qr&chl=VALUE_GOES_HERE&choe=UTF-8
+<?php
+//https://chart.googleapis.com/chart?chs=300x300&cht=qr&chl=VALUE_GOES_HERE&choe=UTF-8
 
 ?>
 
