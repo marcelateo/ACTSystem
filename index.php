e8473c29660d0436d831c4126cfc118a712530d2
Marcelina Teodoro <61193617+marcelateo@users.noreply.github.com>
2021-05-12 11:40:46 +0100

    Update index.php

--- index.php
+++ index.php
@@ -28,7 +28,7 @@ $data6 = mysqli_query($con, $query6);
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title> ACT System</title>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" />
-  <link rel="stylesheet" type="text/css" href="style.css">
+  <link rel="stylesheet" type="text/css" href="css/style.css">
 
   <!-- style for three pages when loading on the same page -->
 
